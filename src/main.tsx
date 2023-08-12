191769c8cdda591f5c2d80f3f485a525a34bf18f
Ivan Molto <ivanmolto@gmail.com>
2023-08-13 01:11:49 +0200

    chore: Configure Livepeer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@rainbow-me/rainbowkit/styles.css";
+import { lightTheme, RainbowKitProvider, Theme } from "@rainbow-me/rainbowkit";
+import { WagmiConfig } from "wagmi";
+import { chains, config } from "./wagmi";
+import {
+  LivepeerConfig,
+  createReactClient,
+  studioProvider,
+} from "@livepeer/react";
+import merge from "lodash.merge";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const liveClient = createReactClient({
+  provider: studioProvider({ apiKey: import.meta.env.VITE_LIVEPEER_API_CORS }),
+});
+
+const myTheme = merge(lightTheme(), {
+  colors: {
+    accentColor: "#4f46e5",
+  },
+} as Theme);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <WagmiConfig config={config}>
+      <LivepeerConfig client={liveClient}>
+        <RainbowKitProvider theme={myTheme} chains={chains}>
+          <App />
+        </RainbowKitProvider>
+      </LivepeerConfig>
+    </WagmiConfig>
   </React.StrictMode>,
-)
+);
